95bf6c7e4c185c9b6373cc1a4ce465f602a9de72
Gaurav <imgauravshrestha@gmail.com>
2025-01-17 16:43:16 +0545

    s,ooth scroll added

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,36 @@ import Program from './pages/Program';
 import Membership from './pages/Membership';
 import Testimonials from './pages/Testimonials';
 import Footer from './pages/Footer';
+import Lenis from "@studio-freight/lenis";
+import { useEffect } from 'react';
 
 function App() {
+
+  // Initialize Lenis for smooth scrolling
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    lenis.on("scroll", handleScroll);
+
+    const raf = (time) => {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    };
+
+    requestAnimationFrame(raf);
+
+    // Cleanup to avoid memory leaks
+    return () => {
+      lenis.off("scroll", handleScroll);
+    };
+  }, []);
+
+  // Scroll event handler
+  const handleScroll = (e) => {
+    console.log(e, "Scroll event triggered");
+  };
   return (
-    <div className='w-full overflow-x-hidden p-0 m-0'>
+    <div id='main' className='w-full overflow-x-hidden p-0 m-0'>
       <Navbar />
       <main>
         <HomePage />
